1476594a3f6053e8c3b4bf4d91eb63a2236a464e
mohbadar <mohammadbadarhashimi@gmail.com>
2020-07-16 13:36:25 +0430

    province

--- pista-core/src/main/java/af/gov/anar/pista/province/Province.java
+++ pista-core/src/main/java/af/gov/anar/pista/province/Province.java
@@ -1,6 +1,7 @@
 package af.gov.anar.pista.province;
 
 import af.gov.anar.pista.infrastructure.base.BaseEntity;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 import javax.persistence.Column;
@@ -18,8 +19,10 @@ import javax.persistence.Table;
 public class Province extends BaseEntity {
 
     @Column(name = "name", nullable = false)
+    @ApiModelProperty(notes = "Province Name")
     private String name;
 
     @Column(name = "province_code", nullable = false, unique = true)
+    @ApiModelProperty(notes = "Province Code")
     private String provinceCode;
 }
